cd133e13ea0004ba9c79cb1c74180dcc8eea1cfd
Thetips4you <51883957+shazforiot@users.noreply.github.com>
2021-08-31 21:29:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello From Thetips4you" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": "It works!" }');
+    res.send('{ "response": " Great!, It works!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;

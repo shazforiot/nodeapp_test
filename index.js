c0de38c9ba3b4d955bb7a07f37141629a2e777f6
Thetips4you <51883957+shazforiot@users.noreply.github.com>
2021-08-31 21:41:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
-app.get('/will', function (req, res) {
+
+app.get('/', function (req, res) {
     res.send('{ "response": "Hello From Thetips4you" }');
+});
+
+app.get('/will', function (req, res) {
+    res.send('{ "response": "This is from page will" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');

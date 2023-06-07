c2a70a2c5c8c9591b18e7b6bea695d685d41660b
Pritesh Mahajan <105488969+pritesh3698@users.noreply.github.com>
2023-06-07 17:57:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello FROM PRITESH MAHAJAN');
+    res.send('..............................................HELLO FROM PRITESH MAHAJAN...............................................');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('...................................................Hello World..........................................................');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('Great!, It works!');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;

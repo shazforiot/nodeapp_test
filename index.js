aa9d3f4603c037e37f9d3814188a86d4851dc85f
Pritesh Mahajan <105488969+pritesh3698@users.noreply.github.com>
2023-06-07 17:48:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello FROM PRITESH MAHAJAN" }');
+    res.send('Hello FROM PRITESH MAHAJAN');
 });
 
 app.get('/will', function (req, res) {

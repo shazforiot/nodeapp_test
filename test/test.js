228bb9efbc72a4599a4fa30d7ffeea3040a552c3
Pritesh Mahajan <105488969+pritesh3698@users.noreply.github.com>
2023-06-07 15:31:16 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var request = require('supertest');
 var app = require('../index.js');
 describe('GET /will', function() {
     it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World" }', done);
+        request(app).get('/will').expect('{ "response": "Hello from pritesh" }', done);
     });
 });

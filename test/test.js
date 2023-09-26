7f0896e248bd93508d93751f1ab2109beac3fe99
GaneshRevanthGit <74895811+ganeshrevanth123@users.noreply.github.com>
2023-09-26 22:44:33 +0530

    Delete test directory

--- test/test.js
+++
@@ -1,7 +0,0 @@
-var request = require('supertest');
-var app = require('../index.js');
-describe('GET /will', function() {
-    it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World" }', done);
-    });
-});

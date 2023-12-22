4a27c60b9696af6fa025ddfe75aa740782cc3801
mnagaraju5628 <124573100+mnagaraju5628@users.noreply.github.com>
2023-12-22 18:49:23 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /will', function() {
-    it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World" }', done);
+    it('respond with Welcome', function(done) {
+        request(app).get('/will').expect('{ "response": "Welcome" }', done);
     });
 });

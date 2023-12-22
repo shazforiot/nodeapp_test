7bb51c066230f7d5cf5729896810dedab1457c0e
mnagaraju5628 <124573100+mnagaraju5628@users.noreply.github.com>
2023-12-22 18:47:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Welcome" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');

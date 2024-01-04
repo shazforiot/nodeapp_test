2f902d383e663412abd69852b037a1691f8308ae
mnagaraju5628 <124573100+mnagaraju5628@users.noreply.github.com>
2024-01-04 16:20:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Welcome to INNODATA-TESTPROJECT " }');
+    res.send('{ "response": "Welcome to Inno-demoproject " }');
 });
 
 app.get('/will', function (req, res) {

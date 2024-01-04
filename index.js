ae22ab1dd82b212c8df90c7246979b2ab4f039af
mnagaraju5628 <124573100+mnagaraju5628@users.noreply.github.com>
2024-01-04 15:29:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Welcome to innotestproject" }');
+    res.send('{ "response": "Welcome to inno-testproject" }');
 });
 
 app.get('/will', function (req, res) {

4b2d5577cfdbbe0a9cd3dd6bfe91f7c73b89bc92
mnagaraju5628 <124573100+mnagaraju5628@users.noreply.github.com>
2024-01-08 09:48:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Welcome to Git-Hub Actions Gcp-test project " }');
+    res.send('{ "response": "Welcome to Git-Hub Actions" }');
 });
 
 app.get('/will', function (req, res) {

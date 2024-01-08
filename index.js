42e462b371aa5cb69d0fd75538f29b50e5b6f6f7
mnagaraju5628 <124573100+mnagaraju5628@users.noreply.github.com>
2024-01-08 10:02:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Welcome to TESTGCP-PROJECT" }');
+    res.send('{ "response": "Welcome to Horizon GCP Project" }');
 });
 
 app.get('/will', function (req, res) {
